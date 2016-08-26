c433890308707370a76a90aef73f76b390d1031a
chengliangkaka <1079914652@qq.com>
2016-08-26 16:55:52 +0800

    添加楼层统计数据

---
+++ Prepaid/Controllers/AnalysisesController.cs
@@ -0,0 +1,40 @@
+﻿using Prepaid.Models;
+using Prepaid.Repositories;
+using Prepaid.Utils;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace Prepaid.Controllers
+{
+    public class AnalysisesController : ApiController
+    {
+        IDeviceRepository deviceRepository;
+
+        public AnalysisesController(IDeviceRepository deviceRepository)
+        {
+            this.deviceRepository = deviceRepository;
+        }
+
+        [Route("api/buildingstatis")]
+        [HttpGet]
+        [ResponseType(typeof(BuildingStatis))]
+        public IHttpActionResult GetBuildingStatis()
+        {
+            var errResult = TextHelper.CheckAuthorized(Request);
+            if (errResult != null)
+                return errResult;
+
+            BuildingStatis statis = this.deviceRepository.GetBuildingStatisInfo();
+
+            return Ok(statis);
+        }
+    }
+}
---
+++ Prepaid/Models/BuildingStatisItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Prepaid.Models
+{
+    public class BuildingStatis
+    {
+        public IEnumerable<string> BuildingNos;
+
+        public IEnumerable<double?> Values;
+    }
+}
